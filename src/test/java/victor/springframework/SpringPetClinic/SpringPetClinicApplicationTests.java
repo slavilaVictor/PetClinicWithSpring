407317470638b03c7061a3dc2d280555e0ef6992
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-01-25 20:25:31 +0200

    First commit

---
+++ src/test/java/victor/springframework/SpringPetClinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.springframework.SpringPetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
