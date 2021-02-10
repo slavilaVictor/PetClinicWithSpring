16ec523556cc61f909efc8f244a3de7b2a4e4128
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-10 11:32:42 +0200

    Adding Base Entity

--- pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/Person.java
+++ pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/Person.java
@@ -1,6 +1,6 @@
 package victor.springframework.SpringPetClinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/PetType.java
+++ pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/PetType.java
@@ -1,6 +1,6 @@
 package victor.springframework.SpringPetClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
