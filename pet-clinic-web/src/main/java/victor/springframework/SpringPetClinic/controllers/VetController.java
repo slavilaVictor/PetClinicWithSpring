58be4e2b756bbee8612aab93d853b315440042c1
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-11 16:51:55 +0200

    Added Vet List and Updated the Controller

--- pet-clinic-web/src/main/java/victor/springframework/SpringPetClinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/victor/springframework/SpringPetClinic/controllers/VetController.java
@@ -1,15 +1,25 @@
 package victor.springframework.SpringPetClinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import victor.springframework.SpringPetClinic.services.VetService;
+
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     // I could use only one path, but I chose multiple for my users
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
 
+        model.addAttribute("vets",vetService.findAll());
 
         // It corresponds to the folder vets, in which I have my index.html
         return "vets/index";
