dd53620796cbcb62a9fa2aaa0d2548ab13305c81
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-11 12:25:04 +0200

    Adding Index Page and Controller

---
+++ pet-clinic-web/src/main/java/victor/springframework/SpringPetClinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package victor.springframework.SpringPetClinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    // When I have a request on this pages
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+
+        // Spring will look for a template named index. And I do have an index.html file
+        return "index";
+    }
+}
