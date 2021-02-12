8839737d163399ba3b6bc6d72aa40dcfa80b937c
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-12 16:27:36 +0200

    Adding Web Resources + Layout to Index Page

--- pet-clinic-web/src/test/java/SpringPetClinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/SpringPetClinic/SpringPetClinicApplicationTests.java
@@ -1,9 +1,12 @@
 package SpringPetClinic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import victor.springframework.SpringPetClinic.SpringPetClinicApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringPetClinicApplication.class)
 class SpringPetClinicApplicationTests {
 
 	@Test
