6156e8c4f377335c1846beb76210e8a535636a5d
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-15 14:10:41 +0200

    Added PetTypes to Bootstrap

--- pet-clinic-web/src/main/java/victor/springframework/SpringPetClinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/victor/springframework/SpringPetClinic/bootstrap/DataLoader.java
@@ -1,11 +1,12 @@
 package victor.springframework.SpringPetClinic.bootstrap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import victor.springframework.SpringPetClinic.model.Owner;
+import victor.springframework.SpringPetClinic.model.PetType;
 import victor.springframework.SpringPetClinic.model.Vet;
 import victor.springframework.SpringPetClinic.services.OwnerService;
+import victor.springframework.SpringPetClinic.services.PetTypeService;
 import victor.springframework.SpringPetClinic.services.VetService;
 
 // By saying that this is a Component, it becomes a Spring Bean and it is registered in the Spring Context
@@ -14,14 +15,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, OwnerService ownerService1, VetService vetService) {
+    public DataLoader(OwnerService ownerService, OwnerService ownerService1, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService1;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
