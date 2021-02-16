72305171053b595939d429c2bc2a1b812dd3efbd
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-16 15:53:10 +0200

    Updated base entity with JPA annotations

--- pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package victor.springframework.SpringPetClinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// Establish this as a base class for JPA
+// The classes from my model inherits this class, but this class is not mapped in my database
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
