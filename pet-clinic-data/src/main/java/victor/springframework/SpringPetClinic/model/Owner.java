c3278c1a27b00b139a88dc38a0eb2ef487e6fc9a
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-16 16:06:38 +0200

    Updated owner with JPA entity mappings

--- pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/Owner.java
+++ pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/Owner.java
@@ -1,13 +1,24 @@
 package victor.springframework.SpringPetClinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    // CascateTyp.ALL -> If I delete an owner, the animals that the owns are deleted too
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
