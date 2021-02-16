0b98fe66050bb3da33b44279985816946f61d443
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-16 18:12:47 +0200

    Implemented Map Implementation for Visit Service

---
+++ pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package victor.springframework.SpringPetClinic.services;
+
+import victor.springframework.SpringPetClinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
