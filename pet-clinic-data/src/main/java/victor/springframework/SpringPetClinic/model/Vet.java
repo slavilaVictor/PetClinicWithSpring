21f382c2045fa08480e355ffa9716fdd4f7c7fc1
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-02-16 16:19:14 +0200

    Updated vet to use JPA

--- pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/Vet.java
+++ pet-clinic-data/src/main/java/victor/springframework/SpringPetClinic/model/Vet.java
@@ -1,10 +1,18 @@
 package victor.springframework.SpringPetClinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    // By default, it is lazy initialized
+    // When I do it eager, JPA will load this all at once
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
